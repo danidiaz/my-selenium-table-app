5e7e014681308852caafbe5689d9fb380642a266
Daniel Diaz <diaz.carrete@facebook.com>
2017-05-01 23:58:12 +0200

    JUnit 4 test.

--- src/test/java/info/danidiaz/selenium/AppTest.java
+++ src/test/java/info/danidiaz/selenium/AppTest.java
@@ -1,38 +1,14 @@
 package info.danidiaz.selenium;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testFoo() {
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
