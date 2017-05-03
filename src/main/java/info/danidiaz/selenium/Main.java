a4f0cdaed44babb1543d14f262d27321050916fc
Daniel Diaz <diaz.carrete@facebook.com>
2017-05-03 10:09:39 +0200

    Table body locator.

--- src/main/java/info/danidiaz/selenium/Main.java
+++ src/main/java/info/danidiaz/selenium/Main.java
@@ -1,7 +1,12 @@
 package info.danidiaz.selenium;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Hello world!
@@ -12,8 +17,15 @@ public class Main
     public static void main( String[] args )
     {
         final String url = args[0];
-        System.out.println(url);
+        final By byTableBody = By.id("bigtablebody");
+
         final RemoteWebDriver driver = new ChromeDriver(); 
+        driver.get(url);
+        wait(driver).until(visibilityOfElementLocated(byTableBody));
         driver.quit();
+    }
+    
+    private static WebDriverWait wait(WebDriver driver) {
+        return new WebDriverWait(driver,10);
     }
 }
